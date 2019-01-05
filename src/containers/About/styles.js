50ef950c7448c741b8f7d946522e66d0016c0d33
Spencer Tiberi <spencertiberi@gmail.com>
2019-01-05 14:53:57 -0800

    Add prelim about copy

--- src/containers/About/styles.js
+++ src/containers/About/styles.js
@@ -1,2 +1,15 @@
 import styled from 'styled-components'
 import { Flex } from 'grid-styled'
+
+export const Container = styled(Flex)`
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Text = styled(Flex)`
+  font-size: 1em;
+  font-family: ${({ theme }) => theme.fonts.main};
+  color: ${({ theme }) => theme.colors.mainFont};
+  text-align: justify;
+  padding: 1em 15vw 1em 15vw;
+`
