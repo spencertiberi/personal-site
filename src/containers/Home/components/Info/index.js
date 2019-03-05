cf85eb4e698f7899fd622043bd42fd8bb3c1feee
Spencer Tiberi <spencertiberi@gmail.com>
2019-03-04 19:41:21 -0500

    Add copy file and abstract

--- src/containers/Home/components/Info/index.js
+++ src/containers/Home/components/Info/index.js
@@ -1,36 +1,16 @@
 import React from 'react'
 import { Container, Text, Title, Paragraph, Pic } from './styles'
 import ProfilePic from './Snowboarding.jpg'
+import copy from '../../../../copy'
 
 const Info = () => (
   <Container>
-    <Title> {`Hi, I'm Spencer and I make things`} </Title>
+    <Title> {copy.hello} </Title>
     <Paragraph>
       <span>
         <Pic src={ProfilePic} alt="Spencer" />
       </span>
-      <Text>
-        Spencer Lee Tiberi was born on March 29th, 1991 in <s>Sparks</s> Las
-        Vegas, Nevada. He grew up in the southwestern Las Vegas suburb of
-        Enterprise, frequently alternating between exploring his desert habitat
-        and venturing into virtual worlds on his Nintendo 64. As a fun
-        juxtaposition to Las Vegas summers, he started snowboarding at the age
-        of 8. After a high school experience spent dabbling in creating music
-        and playing bass guitar, he graduated from the University of Nevada, Las
-        Vegas in 2013 with a BS in Secondary Education - Mathematics and a minor
-        in Psychology (Why though? - A advanced degree in Psychology may be
-        required to figure this one out). He quickly jumped into teaching
-        Mathematics at Mojave High School in North Las Vegas and later at
-        Durango High School closer to home in Enterprise. After four years of
-        teaching in his hometown, he graduated from Harvard Extension School
-        with a Master of Liberal Arts in Mathematics for Teaching and moved out
-        to the Boston area to teach Computer Science at Belmont Hill School
-        utilizing Harvard’s CS50 curriculum. He now works as an Engineering
-        Manager at Harvard, helping undergrads develop web and mobile apps for
-        entrepreneurial clients. To this day, he still plays bass guitar,
-        snowboards, plays video games, and explores his habitat (desert or
-        otherwise).
-      </Text>
+      <Text>{copy.bio}</Text>
     </Paragraph>
   </Container>
 )
---
+++ src/copy.js
@@ -0,0 +1,7 @@
+const copy = {
+  bio:
+    'Spencer Lee Tiberi was born on March 29th, 1991 in Sparks, Nevada. However, he spent most of his life in Las Vegas. Spencer grew up in the southwestern Las Vegas suburb of Enterprise, frequently alternating between exploring his desert habitat and venturing into virtual worlds on his Nintendo 64. As a fun juxtaposition to Las Vegas summers, he started snowboarding at the age of 8. After a high school experience spent dabbling in creating music and playing bass guitar, Spencer graduated from the University of Nevada, Las Vegas in 2013 with a BS in Secondary Education - Mathematics and a minor in Psychology. He quickly jumped into teaching Mathematics at Mojave High School in North Las Vegas and later at Durango High School closer to his home in Enterprise. After four years of teaching in his hometown, Spencer graduated from Harvard Extension School with a Master of Liberal Arts in Mathematics for Teaching and moved out to the Boston area to teach Computer Science at Belmont Hill School utilizing Harvard’s CS50 curriculum. He now works as an Engineering Manager at Harvard, helping undergrads develop web and mobile apps for entrepreneurial clients. To this day, he still plays bass guitar, snowboards, plays video games, and explores his habitat (desert or otherwise).',
+  hello: "Hi, I'm Spencer and I make things"
+}
+
+export default copy
