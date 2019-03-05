e5bdfb7a84f04cf4ba88977edaedc0e597dcfa6d
Spencer Tiberi <spencertiberi@gmail.com>
2019-03-04 19:43:42 -0500

    Update background image and add lazy load to logo

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import Heading from '../../components/Heading'
 import { Container, MainImage } from './styles'
-import backgroundImage from './SpringMountains.jpg'
+import backgroundImage from './TeaTime.jpg'
 import Info from './components/Info'
+import LazyLoad from 'react-lazy-load'
 
 class Home extends Component {
   render() {
     return (
       <Container>
-        <Heading scroll> Spencer Tiberi </Heading>
-        <MainImage src={backgroundImage} alt="Spencer" />
+        <Heading scroll> Spencer Lee Tiberi </Heading>
+        <LazyLoad height={400}>
+          <MainImage src={backgroundImage} alt="Spencer" />
+        </LazyLoad>
         <Info />
       </Container>
     )
